a053cadfba8b6689740ad33ccc1825a94bac78ae
DwashKarma <dwaskc@gmail.com>
2024-04-19 15:16:28 +0545

    acbavjc

--- dist/index.esm.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-var MyComponent = function (_a) {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "Hello")));
-};
-
-export { MyComponent as default };
--- dist/types/Accordian.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-interface MyComponentProps {
-}
-declare const MyComponent: React.FC<MyComponentProps>;
-export default MyComponent;
